b75a123b22b92c33c1a19e8c584832b32b87b82f
barcohen10 <barcohen10@gmail.com>
2015-09-18 17:27:27 +0300

    get songs from json and add it to html
    Signed-off-by: barcohen10 <barcohen10@gmail.com>

--- MusicPlayer/js/mainScript.js
+++ MusicPlayer/js/mainScript.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(document).ready(function () {
+    var allSongs = [];
+
+    (function getAllSongsFromJSON()
+    {
+        $.getJSON("http://shmulik.mybluemix.net/playlist")
+.done(function (json) {
+    json = json.response.songs;
+    for (var i = 0; i < json.length; i++) {
+        allSongs.push(new Song(json[i].artist_name, json[i].title));
+        $("#plList").append("<li> <div class='plItem'> <div class='plNum'>" + (i+1) + ".</div> <div class='plTitle'>" + json[i].artist_name + " - "  + json[i].title + " </div> </div> </li>");
+    }
+
+});
+    })();
+
+
+});
