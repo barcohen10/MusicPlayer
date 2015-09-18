3e87aafbd8b4f7742dd40f59583a7eb60c8820b2
barcohen10 <barcohen10@gmail.com>
2015-09-18 16:39:50 +0300

    create song class
    Signed-off-by: barcohen10 <barcohen10@gmail.com>

---
+++ MusicPlayer/js/song.js
@@ -0,0 +1,17 @@
+﻿function Song(artistName, songTitle) {
+    this.artistName = artistName;
+    this.songTitle = songTitle;
+    this.url = "http://toma.hk/embed.php?artist=" + artistName + "&title=" + songTitle + "&autoplay=true";
+}
+
+Song.prototype.getArtistName = function () {
+    return this.artistName;
+};
+
+Song.prototype.getSongTitle = function () {
+    return this.songTitle;
+};
+
+Song.prototype.getUrl = function () {
+    return this.url;
+};
